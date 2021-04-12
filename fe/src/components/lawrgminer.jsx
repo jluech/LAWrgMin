1a89cbc9a55b14d3d6c7c6ca19d141758e16b818
Janik Lüchinger <janik.luechinger@uzh.ch>
2021-04-12 14:48:09 +0200

    refactor: cleanup and reformat files

--- fe/src/components/lawrgminer.jsx
+++ fe/src/components/lawrgminer.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import {report} from "./data";
-import uzh from "../images/uzh.svg";
 
 class Lawrgminer extends React.Component {
     render() {
-        return <div id='lawrgminer' style={{backgroundColor:'lightgray'}}>
+        return <div id='lawrgminer' style={{backgroundColor: 'lightgray'}}>
             <h2 id='introduction' className="section">LAWrgMiner</h2>
             <h3 className="section">Argumentation Mining in Law</h3>
-            <div >
+            <div>
                 <h4 className='section'>1. Text Input</h4>
-                <hr className="solid" style={{position:'relative', top:'1em', left:'3em'}}/>
+                <hr className="solid" style={{position: 'relative', top: '1em', left: '3em'}}/>
 
             </div>
         </div>;
