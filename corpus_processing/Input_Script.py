90512b837725763c3e1a3951fbc1cfcde1bf399f
Janik Lüchinger <janik.luechinger@uzh.ch>
2021-04-14 14:19:24 +0200

    refactor: extend extraction script
    - properly manage dependencies

--- corpus_processing/Input_Script.py
+++
@@ -1,9 +0,0 @@
-# To use textract package install poppler
-conda install -c conda-forge poppler
-pip install textract
-import textract
-
-# Function to read PDF, Word or HTML Documents 
-def get_text(filepath):
-    text = textract.process(filepath)
-    return text.decode('utf8')
