a53e5c09430d811786fc400f600855a84f2e6222
schmiddam <damaris.schmid2@uzh.ch>
2021-04-17 17:21:35 +0200

    File upload frontend more or less done

--- frontend/src/components/reportSection.js
+++ frontend/src/components/reportSection.js
@@ -4,12 +4,12 @@ import uzh from "../images/uzh.svg";
 import "./App.css";
 import {report} from "./data.js";
 
-class ReportSection extends React.Component {
+export class ReportSection extends React.Component {
     render() {
         return (
             <div>
                 <div className="light">
-                    <h2 id="introduction" className="section">
+                    <h2 id="introduction" className="section-title">
                         {report[0].section}
                     </h2>
                     <div className="textbox">
@@ -18,7 +18,7 @@ class ReportSection extends React.Component {
                     </div>
                 </div>
                 <div className="dark">
-                    <h2 className="section">{report[1].section}</h2>
+                    <h2 className="section-title">{report[1].section}</h2>
                     <div className="textbox">
                         <img className="text-image" src={uzh} alt={"uzh_logo"} />
                         <p className="text">{report[1].text}</p>
@@ -28,5 +28,3 @@ class ReportSection extends React.Component {
         );
     }
 }
-
-export default ReportSection;
