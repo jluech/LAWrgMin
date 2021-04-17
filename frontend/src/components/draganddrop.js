b18824a749a3c350046a04aaccdf5286d927d921
schmiddam <damaris.schmid2@uzh.ch>
2021-04-17 15:26:20 +0200

    Fileupload inserted

---
+++ frontend/src/components/draganddrop.js
@@ -0,0 +1,126 @@
+import React from 'react';
+
+
+export class DragAndDrop extends React.Component {
+    dropRef = React.createRef()
+
+    //constructor() {
+    //    super();
+    //    this.state = {
+    //        dragging: false
+    //    }
+    //}
+
+
+    state = {
+        dragging: false
+    }
+    handleDrag = (e) => {
+        // prevents the default behavior of the browser when something is dragged in or dropped (e.g. open the file )
+        e.preventDefault()
+        // stops the event from being propagated through parent and child elements
+        e.stopPropagation()
+    }
+
+    handleDragIn = (e) => {
+        // prevents the default behavior of the browser when something is dragged in or dropped (e.g. open the file )
+        e.preventDefault()
+        // stops the event from being propagated through parent and child elements
+        e.stopPropagation()
+        // want to keep track of the how many elements deep our cursor is, and only set call this.setState({dragging: false}) once our cursor is all the way out
+        this.dragCounter++
+
+        if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+            this.setState({dragging: true})
+        }
+    }
+
+    handleDragOut = (e) => {
+        // prevents the default behavior of the browser when something is dragged in or dropped (e.g. open the file )
+        e.preventDefault()
+        // stops the event from being propagated through parent and child elements
+        e.stopPropagation()
+
+        // // want to keep track of the how many elements deep our cursor is, and only set call this.setState({dragging: false}) once our cursor is all the way out
+        this.dragCounter--
+        if (this.dragCounter > 0) return
+
+        this.setState({dragging: false})
+    }
+
+    handleDrop = (e) => {
+        // prevents the default behavior of the browser when something is dragged in or dropped (e.g. open the file )
+        e.preventDefault()
+        // stops the event from being propagated through parent and child elements
+        e.stopPropagation()
+
+        // hide the overlay,
+        // check that there are indeed some files included,
+        // pass the array to our callback,
+        // clear the dataTransfer array,
+        // and reset the drag counter
+        this.setState({drag: false})
+        if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+            this.props.handleDrop(e.dataTransfer.files)
+            e.dataTransfer.clearData()
+            this.dragCounter = 0
+        }
+    }
+
+    componentDidMount() {
+        this.dragCounter = 0
+        let div = this.dropRef.current
+        div.addEventListener('dragenter', this.handleDragIn)
+        div.addEventListener('dragleave', this.handleDragOut)
+        div.addEventListener('dragover', this.handleDrag)
+        div.addEventListener('drop', this.handleDrop)
+    }
+
+    componentWillUnmount() {
+        let div = this.dropRef.current
+        div.removeEventListener('dragenter', this.handleDragIn)
+        div.removeEventListener('dragleave', this.handleDragOut)
+        div.removeEventListener('dragover', this.handleDrag)
+        div.removeEventListener('drop', this.handleDrop)
+    }
+
+    render() {
+        return (
+            <div ref={this.dropRef}>
+                {this.props.children}
+
+                {this.state.dragging &&
+                <div
+                    style={{
+                        border: 'dashed grey 4px',
+                        backgroundColor: 'rgba(255,255,255,.8)',
+                        position: 'absolute',
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        zIndex: 9999
+                    }}
+                >
+                    <div
+                        style={{
+                            position: 'absolute',
+                            top: '50%',
+                            right: 0,
+                            left: 0,
+                            textAlign: 'center',
+                            color: 'grey',
+                            fontSize: 36
+                        }}
+                    >
+                        <div>drop here :)</div>
+                    </div>
+                </div>
+                }
+                {this.props.children}
+            </div>
+
+        )
+    }
+}
+
