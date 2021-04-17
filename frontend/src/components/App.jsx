075c5fae8118d34d800c6f0f7f543d938117709f
schmiddam <damaris.schmid2@uzh.ch>
2021-04-17 15:26:20 +0200

    Fileupload inserted

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,25 +1,26 @@
-import React from "react";
+import './App.css';
+import { report } from './data.js';
+import {Header} from "./header";
+import {Lawrgminer} from './lawrgminer';
+import Button from 'react-bootstrap/Button';
+import React from 'react';
+import {Footer} from "./footer";
+import {ReportSection} from "./reportSection";
 
-import "./App.css";
-import Header from "./header";
-import Lawrgminer from "./lawrgminer";
-import Footer from "./footer";
-import ReportSection from "./reportSection";
 
-class App extends React.Component {
+export class App extends React.Component {
+
     render() {
-        return (
-            <div className="App">
-                <Header />
-                <body>
-                    <ReportSection />
-                    <Lawrgminer />
-                </body>
+        return <div className="App">
 
-                <Footer />
-            </div>
-        );
+            <Header />
+            <body>
+                <ReportSection />
+                <Lawrgminer />
+            </body>
+
+            <Footer />
+        </div>
     }
 }
 
-export default App;
--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,32 +1,22 @@
-import React from "react";
+import footer from '../images/footerimage.jpg';
+import './App.css';
+import React from 'react';
 
-import "./App.css";
-import footer from "../images/footerimage.jpg";
+export class Footer extends React.Component {
 
-class Footer extends React.Component {
     render() {
-        return (
-            <div className="App">
-                <footer
-                    className="App-footer"
-                    style={{
-                        backgroundImage: `url(${footer})`,
-                    }}
-                >
-                    <div className="header-footer-content-box" style={{position: "relative", left: "10%"}}>
-                        <h3>AI Technology and Law</h3>
-                        <hr className="solid" />
-                        <h6>Course: AI: Technology and Law (FS21)</h6>
-                        <h6>Lecturers: Prof. Abraham Bernstein PhD & Prof. Dr. Florent Thouvenin</h6>
-                        <h6>
-                            The research project was executed by Tjasa Marincek, Janik Lüchinger, Damaris Schmid, Cédric
-                            Zellweger & Adrian Zermin
-                        </h6>
-                    </div>
-                </footer>
-            </div>
-        );
+        return <div className="App">
+            <footer className="App-footer" style= { {
+                backgroundImage: `url(${footer})`
+            }}>
+                <div className="header-footer-content-box" style={{position:"relative", left:"10%"}}>
+                    <h3>AI Technology and Law</h3>
+                    <hr className="solid"/>
+                    <h6>Course: AI: Technology and Law (FS21)</h6>
+                    <h6>Lecturers: Prof. Abraham Bernstein PhD & Prof. Dr. Florent Thouvenin</h6>
+                    <h6>The research project was executed by Tjasa Marincek, Janik Lüchinger, Damaris Schmid, Cédric Zellweger & Adrian Zermin</h6>
+                </div>
+            </footer>
+        </div>
     }
 }
-
-export default Footer;
