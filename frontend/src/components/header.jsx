3ad372d005bcdebaa0181eb8da92c521fc102f11
schmiddam <damaris.schmid2@uzh.ch>
2021-04-17 15:26:20 +0200

    Fileupload inserted

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,63 +1,50 @@
 import React from "react";
-import {NavLink} from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import {Link} from "react-scroll";
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import header_img from "../images/header_img.jpg";
 import uzh from "../images/uzh.svg";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {NavLink} from "react-bootstrap";
+import {Link} from 'react-scroll';
 
-class Header extends React.Component {
+
+
+
+export class Header extends React.Component {
+
     render() {
-        return (
-            <div className="App">
-                <header
-                    className="App-header"
-                    style={{
-                        backgroundImage: `url(${header_img})`,
-                    }}
-                >
+        return <div className="App">
+            <header className="App-header" style= { {
+                backgroundImage: `url(${header_img})`
+            }}>
+                <div>
+                    <img src={uzh} alt={""}/>
+                    <ul style={{
+                        listStyle: 'none',
+                        display: 'inline-flex',
+                        position:'fixed',
+                        right:'10px'
+                    }}>
+                        <NavLink />
+                        <li style={{padding: '2em'}}>
+                            <Link activeClass="active" to="introduction" spy={true} smooth={true}>Project</Link>
+                        </li>
+                        <li style={{padding: '2em'}}>
+                            <Link activeClass="active" to="lawrgminer" spy={true} smooth={true}>LAWrgMiner</Link>
+                        </li>
+                    </ul>
+                    <h1 style={{paddingTop: '8em', paddingLeft:'2em'}}>LAWrgMiner Project</h1>
+                    <hr className="solid" style={{position:'relative', top:'1em', left:'3em'}}/>
+                    <h6 style={{position:'relative', top:'2em', left:'5em'}}>Artificial Intelligence: Technology and Law</h6>
+                    <Button style={{position:'relative', top:'3em', left:'5em'}} variant="outline-light" >Use LAWrgMiner</Button>
                     <div>
-                        <img src={uzh} alt={""} />
-                        <ul
-                            style={{
-                                listStyle: "none",
-                                display: "inline-flex",
-                                position: "fixed",
-                                right: "10px",
-                            }}
-                        >
-                            <NavLink />
-                            <li style={{padding: "2em"}}>
-                                <Link activeClass="active" to="introduction" spy={true} smooth={true}>
-                                    Project
-                                </Link>
-                            </li>
-                            <li style={{padding: "2em"}}>
-                                <Link activeClass="active" to="lawrgminer" spy={true} smooth={true}>
-                                    LAWrgMiner
-                                </Link>
-                            </li>
-                        </ul>
-                        <h1 style={{paddingTop: "8em", paddingLeft: "2em"}}>LAWrgMiner Project</h1>
-                        <hr className="solid" style={{position: "relative", top: "1em", left: "3em"}} />
-                        <h6 style={{position: "relative", top: "2em", left: "5em"}}>
-                            Artificial Intelligence: Technology and Law
-                        </h6>
-                        <Button style={{position: "relative", top: "3em", left: "5em"}} variant="outline-light">
-                            Use LAWrgMiner
-                        </Button>
-                        <div>
-                            <br />
-                            <br />
-                            <br />
-                            <br />
-                        </div>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
                     </div>
-                </header>
-            </div>
-        );
+                </div>
+            </header>
+        </div>
     }
 }
 
-export default Header;
--- frontend/src/components/lawrgminer.jsx
+++ frontend/src/components/lawrgminer.jsx
@@ -1,20 +1,51 @@
-import React from "react";
+import React from 'react';
+import {FileUpload} from "./fileupload";
+import {DragAndDrop} from "./draganddrop";
 
-class Lawrgminer extends React.Component {
+
+
+export class Lawrgminer extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            files: [
+                'testfile.pdf'
+            ]
+        }
+    }
+
+    handleDrop = (files) => {
+        let fileList = this.state.files
+        for (let i = 0; i < files.length; i++) {
+            if (!files[i].name) return
+            fileList.push(files[i].name)
+        }
+        this.setState({files: fileList})
+    }
+
     render() {
-        return (
-            <div id="lawrgminer" style={{backgroundColor: "lightgray"}}>
-                <h2 id="introduction" className="section">
-                    LAWrgMiner
-                </h2>
-                <h3 className="section">Argumentation Mining in Law</h3>
-                <div>
-                    <h4 className="section">1. Text Input</h4>
-                    <hr className="solid" style={{position: "relative", top: "1em", left: "3em"}} />
-                </div>
+        return <div id='lawrgminer' style={{backgroundColor:'lightgray'}}>
+            <h2 id='introduction' className="section">I am the LAWrgMiner</h2>
+            <h3 className="section">Argumentation Mining in Law</h3>
+            <div >
+                <h4 className='section'>1. Text Input</h4>
+                <hr className="solid" style={{position:'relative', top:'1em', left:'3em'}}/>
+
+                <DragAndDrop handleDrop={this.handleDrop} >
+                    <div className='section' style={{height: 300, width: 250}}>
+                        {
+                            this.state.files.map((file) =>
+                            <div key={this.i}>
+                                {file}
+                            </div>
+                        )}
+                    </div>
+                </DragAndDrop>
+                <FileUpload/>
+
             </div>
-        );
+        </div>;
     }
 }
 
-export default Lawrgminer;
