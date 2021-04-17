fa0633279749d0e88d542020a606109fcde8c6b5
jluech <janik@luechinger.ch>
2021-04-17 12:58:06 +0200

    refactor: reformat files acc. to eslint and prettier

--- frontend/src/components/lawrgminer.jsx
+++ frontend/src/components/lawrgminer.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 
 class Lawrgminer extends React.Component {
     render() {
-        return <div id='lawrgminer' style={{backgroundColor: 'lightgray'}}>
-            <h2 id='introduction' className="section">LAWrgMiner</h2>
-            <h3 className="section">Argumentation Mining in Law</h3>
-            <div>
-                <h4 className='section'>1. Text Input</h4>
-                <hr className="solid" style={{position: 'relative', top: '1em', left: '3em'}}/>
-
+        return (
+            <div id="lawrgminer" style={{backgroundColor: "lightgray"}}>
+                <h2 id="introduction" className="section">
+                    LAWrgMiner
+                </h2>
+                <h3 className="section">Argumentation Mining in Law</h3>
+                <div>
+                    <h4 className="section">1. Text Input</h4>
+                    <hr className="solid" style={{position: "relative", top: "1em", left: "3em"}} />
+                </div>
             </div>
-        </div>;
+        );
     }
 }
 
