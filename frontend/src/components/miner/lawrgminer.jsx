a0acac4c7fc5b9b338b3c8faec9467d096badf58
Janik Lüchinger <janik.luechinger@uzh.ch>
2021-04-20 23:51:16 +0200

    refactor: extract most lawrgminer html styles into scss file

--- frontend/src/components/miner/lawrgminer.jsx
+++ frontend/src/components/miner/lawrgminer.jsx
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FileDocumentMultipleOutlineIcon from "mdi-react/FileDocumentMultipleOutlineIcon";
 
+import "./scss/lawrgminer.scss";
 import {FileUpload} from "./fileupload";
 import {DragAndDrop} from "./draganddrop";
 import {Arguments} from "./arguments";
@@ -18,48 +19,38 @@ export class Lawrgminer extends React.Component {
 
     render() {
         return (
-            <div id="lawrgminer" style={{backgroundColor: "#071032"}}>
+            <div className={"lawrgminer"} id="lawrgminer">
                 <h2 className="section-title">I am the LAWrgMiner</h2>
                 <h3 className="section-title">Argumentation Mining in Law</h3>
-                <div>
+                <div className={"miner-input"}>
                     <h4 className="section-title">1. Text Input</h4>
-                    <br />
-                    <span style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <span className={"input-drag-drop"}>
                         <DragAndDrop>
-                            <div
-                                className="section"
-                                style={{
-                                    display: "flex",
-                                    height: 300,
-                                    width: 300,
-                                    border: "dashed grey 4px",
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                }}
-                            >
+                            <div className="section section-drag-drop">
                                 <h3>Drag a PDF here!</h3>
                                 <FileDocumentMultipleOutlineIcon />
                             </div>
                         </DragAndDrop>
-                        <FileUpload className="section" />
+                        <FileUpload className="section section-file-upload" />
                     </span>
                 </div>
                 <br />
+                {/*TODO: refactor to remove br tags and properly style hr*/}
                 <hr className="solid" style={{position: "relative", top: "1em", left: "3em"}} />
                 <br />
-                <div>
+                <div className={"miner-results"}>
                     <h4 className="section-title">2. Results</h4>
-                    <span style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                        <div style={{marginLeft: "10%", marginRight: "5%"}}>
-                            <h5 style={{color: "white"}}>Claims</h5>
+                    <div className={"miner-results-list"}>
+                        <div className={"miner-results-claims"}>
+                            <h5>Claims</h5>
                             <Claims />
                         </div>
-                        <div style={{marginRight: "10%"}}>
-                            <h5 style={{color: "white"}}>Arguments</h5>
+                        <div className={"miner-results-arguments"}>
+                            <h5>Arguments</h5>
                             <Arguments />
                         </div>
-                    </span>
-                    <Button style={{marginLeft: "10%", marginTop: "5%"}} variant="outline-light">
+                    </div>
+                    <Button className={"miner-results-export-btn"} variant="outline-light">
                         Export Data
                     </Button>
                 </div>
