e2dab64eb3dd637fc31dfe29e2d492af56a84c89
Janik Lüchinger <janik.luechinger@uzh.ch>
2021-04-21 16:30:52 +0200

    feat: implement primitive API call for tagWithText

--- frontend/src/components/miner/lawrgminer.jsx
+++ frontend/src/components/miner/lawrgminer.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FileDocumentMultipleOutlineIcon from "mdi-react/FileDocumentMultipleOutlineIcon";
 
-import "../../scss/lawrgminer.scss";
 import {FileUpload} from "./file-upload";
 import {DragAndDrop} from "./drag-and-drop";
 import {Arguments} from "./arguments";
 import {Claims} from "./claims";
+
+
+const api_host = "http://localhost:5000"
 
 export class Lawrgminer extends React.Component {
     constructor() {
@@ -25,6 +28,17 @@ export class Lawrgminer extends React.Component {
     tagWithText() {
         const {inputText} = this.state;
         console.log("tagging input text\n", inputText); // TODO
+
+        const request_url = `${api_host}/api/tagWithText`
+        axios.post(request_url, {"text": inputText})
+            .then((reply) => {
+                console.log(reply.data); // TODO
+                console.log(reply.status); // TODO
+            })
+            .catch((err) => {
+                console.log("error during request:", request_url, "\n", err);
+                alert(`Something went wrong!\nError during request: ${request_url}`);
+            });
     }
 
     render() {
@@ -32,6 +46,7 @@ export class Lawrgminer extends React.Component {
             <div className={"lawrgminer"}>
                 <h2 className="section-title">I am the LAWrgMiner</h2>
                 <h3 className="section-title">Argumentation Mining in Law</h3>
+
                 <h4 className="section-title">1. Text Input</h4>
                 <div className={"miner-input"}>
                     <div className={"input-textarea"}>
@@ -45,33 +60,35 @@ export class Lawrgminer extends React.Component {
                         <DragAndDrop>
                             <div className="section section-drag-drop">
                                 <h3>Drag a PDF here!</h3>
-                                <FileDocumentMultipleOutlineIcon />
+                                <FileDocumentMultipleOutlineIcon/>
                             </div>
                         </DragAndDrop>
-                        <FileUpload className="section section-file-upload" />
+                        <FileUpload className="section section-file-upload"/>
                     </div>
                 </div>
-                <br />
+
+                <br/>
                 {/*TODO: refactor to remove br tags and properly style hr*/}
-                <hr className="solid" style={{position: "relative", top: "1em"}} />
-                <br />
+                <hr className="solid" style={{position: "relative", top: "1em"}}/>
+                <br/>
+
+                <h4 className="section-title">2. Results</h4>
                 <div className={"miner-results"}>
-                    <h4 className="section-title">2. Results</h4>
                     <div className={"miner-results-list"}>
                         <div className={"miner-results-claims"}>
                             <h5>Claims</h5>
-                            <Claims />
+                            <Claims/>
                         </div>
                         <div className={"miner-results-arguments"}>
                             <h5>Arguments</h5>
-                            <Arguments />
+                            <Arguments/>
                         </div>
                     </div>
                     <Button className={"miner-results-export-btn"} variant="outline-light">
                         Export Data
                     </Button>
                 </div>
-                <br />
+                <br/>
             </div>
         );
     }
