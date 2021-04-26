a23f862d528092b9614ec8d57d9fa8cec081f344
Janik Lüchinger <janik.luechinger@uzh.ch>
2021-04-26 23:04:51 +0200

    fix: adjust command when not loading existing model

--- miners/targer_instance/training.py
+++ miners/targer_instance/training.py
@@ -63,8 +63,14 @@ if __name__ == "__main__":
     os.chdir("lstm")  # need to call it relative to the main file
     # so other files being loaded during the process have the correct working directory
 
-    subprocess.run(["python", targer_main_file, "--train", train_file, "--dev", dev_file, "--test", test_file,
+    if (len(load)):
+        subprocess.run(["python", targer_main_file, "--train", train_file, "--dev", dev_file, "--test", test_file,
                     "-d", data_formatting, "--evaluator", evaluation_type, "-e", epochs, load, loadPath, "--opt", "adam",
+                    "--lr", "0.001", "--save-best", "yes", "--patience", "20", "--rnn-hidden-dim", "200",
+                    "--gpu", "-1"])
+    else:
+        subprocess.run(["python", targer_main_file, "--train", train_file, "--dev", dev_file, "--test", test_file,
+                    "-d", data_formatting, "--evaluator", evaluation_type, "-e", epochs, "--opt", "adam",
                     "--lr", "0.001", "--save-best", "yes", "--patience", "20", "--rnn-hidden-dim", "200",
                     "--gpu", "-1"])
     os.chdir("..")
