42ceae73de9b50b062fc86007cc16ab2ead1aa14
Janik Lüchinger <janik.luechinger@uzh.ch>
2021-05-01 17:09:55 +0200

    feat: finalize complete flow for tagWithText

--- backend/__main__.py
+++ backend/__main__.py
@@ -6,6 +6,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
+import examples
 from targer_output_processing import process_targer_output_data
 import utils
 
@@ -41,8 +42,8 @@ def tag_with_text():
     orig_wd = os.getcwd()
     if not os.path.exists(files_dir):
         utils.create_tagging_subdir(file_id)
-    os.chdir(files_dir)
 
+    os.chdir(files_dir)
     file_name = secure_filename("tagging_req_{__id}.txt".format(__id=file_id))
     if os.path.exists(file_name):
         os.remove(file_name)
@@ -55,17 +56,25 @@ def tag_with_text():
         file.write(text)
 
     # Call targer for labelling the new .txt file
-    os.chdir("targer_instance")
+    os.chdir("/".join([orig_wd, "targer_instance"]))
     os.system("python labelling.py -i {__in_dir} -o {__out_dir}".format(__in_dir=files_dir, __out_dir=files_dir))
-    os.chdir("..")
+    os.chdir(orig_wd)
 
     # Read results from targer .out file
     labelled_results = process_targer_output_data(file_id, files_dir)
-    logging.debug(labelled_results)
+    if labelled_results.__len__() > 1:
+        raise RuntimeError("Tagging multiple out files in request folder %d", file_id)
+    # keys: "doc_id", "blocks", "claims", "premises"
+    # TODO: currently "text" instead of "blocks"
+
+    print(labelled_results[0].keys())
 
     # Prepare return in json format.
     return jsonify({
         "id": file_handler.file_id,
+        "blocks": examples.example_blocks,
+        "claims": examples.example_claims,
+        "premises": examples.example_premises,
         "status": "created"
     })
 
