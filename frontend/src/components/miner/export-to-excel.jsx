18bcca5662c06a65ce835fdd56bfc4505e1b4531
Janik Lüchinger <janik.luechinger@uzh.ch>
2021-05-05 18:52:28 +0200

    fix: call exportToCsv only on button click

--- frontend/src/components/miner/export-to-excel.jsx
+++ frontend/src/components/miner/export-to-excel.jsx
@@ -1,46 +1,43 @@
-import React, {useEffect, useState} from "react";
-import FileSaver from "file-saver";
-import XLSX from "xlsx";
+import React, {useState} from "react";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
+import FileSaver from "file-saver";
+import XLSX from "xlsx";
 
 export const ExportToExcel = (props) => {
     const fileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
     const fileExtension = ".xlsx";
     const [exportData, setExportData] = useState(null)
 
-    // The Effect Hook lets you perform side effects in function components
-    // It combines componentDidMount, componentDidUpdate, and componentWillUnmount
-    useEffect(() => {
+    // create download with CSV file from exportData
+    const exportToCSV = () => {
         const request_url = `${props.api_host}/api/exportToCsv/${props.fileId}`
-        const fetchData = () => {
-            axios.get(request_url)
-                // set returned csv data as state {exportData: data}
-                .then((response) => {
-                    setExportData(response)
-                })
-                .catch((err) => {
-                    console.log("error during request:", request_url, "\n", err);
-                    alert(`Something went wrong!\nError during request: ${request_url}`);
-                });
-        }
-        fetchData()
-    }, [])
+        console.log(request_url); // TODO
+        axios.get(request_url)
+            // set returned csv data as state {exportData: data}
+            .then((response) => {
+                console.log(response); // TODO
+                setExportData(response)
 
-    // create Download with CSV file from exportData
-    const exportToCSV = (apiData) => {
-        const ws = XLSX.utils.json_to_sheet(apiData);
-        const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
-        const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-        const data = new Blob([excelBuffer], { type: fileType });
-        // file is saved as e.g. "lawrgminer_export.xlsx"
-        FileSaver.saveAs(data, "lawrgminer_export" + fileExtension);
+                console.log("export csv func", exportData); // TODO
+                const ws = XLSX.utils.json_to_sheet(exportData);
+                const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+                const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+                const data = new Blob([excelBuffer], { type: fileType });
+
+                // file is saved as e.g. "lawrgminer_export.xlsx"
+                FileSaver.saveAs(data, "lawrgminer_export" + fileExtension);
+            })
+            .catch((err) => {
+                console.log("error during request:", request_url, "\n", err);
+                alert(`Something went wrong!\nError during request: ${request_url}`);
+            });
     };
 
     return (
         <Button className={"miner-results-export-btn"}
                 variant="outline-light"
-                onClick={() => exportToCSV(exportData, props.fileId)}
+                onClick={exportToCSV}
         >Export to CSV</Button>
     );
 };
