88f33d575b77102d7eb44d4d1a1d5fd9e4c40335
Janik Lüchinger <janik.luechinger@uzh.ch>
2021-05-06 12:57:26 +0200

    refactor: match labeling format with frontend needs

--- backend/targer_output_processing.py
+++ backend/targer_output_processing.py
@@ -30,40 +30,35 @@ def transform_output_into_csv(file_id, file_dir):
                 word = word_obj["token"]
                 text += "%s%s" % (determine_delimiter(word), word)
         if label in ["Claim", "Premise"]:
-            csv_list.append([idx, text.strip(), label])
+            csv_list.append([idx + 1, text.strip(), label])  # user-friendly 1-based indexing
     return csv_list
 
 
-def processing_helper(stack, counter):
-    block_list = []
-
-    if stack[0]['label'].split('-')[0] == 'O':
-        curr_tag = 'O'
+def processing_helper(stack, clause_counter):
+    if stack[0]["label"].split("-")[0] == "O":
+        curr_label = "O"
     else:
-        curr_tag = stack[0]['label'].split('-')[1]
+        curr_label = stack[0]["label"].split("-")[1]
     clause = {
-        'text': '',
-        'tag': curr_tag,
-        'idx': counter
+        "text": "",
+        "label": curr_label,
+        "idx": clause_counter
     }
-    prev_tag = curr_tag
-    while curr_tag == prev_tag:
-        clause['text'] = clause['text'] + ' ' + stack[0]['token']
-        counter = counter + 1
-        block_dict = {
-            'token': stack[0]['token'],
-            'label': curr_tag,
-            'idx': counter
-        }
-        block_list.append(block_dict)
+
+    # Split clauses based on switching label.
+    prev_label = curr_label
+    while curr_label == prev_label:
+        clause["text"] += "%s%s" % (determine_delimiter(stack[0]["token"]), stack[0]["token"])
+
         stack.pop(0)
         if not len(stack):
             break
-        if stack[0]['label'].split('-')[0] == 'O':
-            curr_tag = 'O'
+        if stack[0]["label"].split("-")[0] == "O":
+            curr_label = "O"
         else:
-            curr_tag = stack[0]['label'].split('-')[1]
-    return block_list, clause, stack, counter
+            curr_label = stack[0]["label"].split("-")[1]
+    clause["text"] = clause["text"].strip()
+    return clause, stack
 
 
 def process_targer_output_data(doc_id, doc_path=targer_output_dir_path):
@@ -74,25 +69,29 @@ def process_targer_output_data(doc_id, doc_path=targer_output_dir_path):
     results = []
     for file in output_files:
         file_dict = {
-            'doc_id': doc_id,
-            'text': '',
-            'premises': [],
-            'claims': [],
-            'blocks': []
+            "doc_id": doc_id,
+            "premises": [],
+            "claims": [],
+            "blocks": []
         }
+
         with open(os.path.join(doc_path, file)) as corpus_file:
-            # ========== load and parse data from json file to dictionary ==========
+            # Load and parse data from json file to dictionary.
             raw_data = json.load(corpus_file)
-            stack = raw_data['results'][0]
-            word_counter = 0
+            stack = raw_data["results"][0]
+
+            clause_counter = 1  # user-friendly 1-based indexing
             while len(stack):
-                block_list, clause, stack, word_counter = processing_helper(stack, word_counter)
-                file_dict['text'] = file_dict['text'] + clause['text']
-                if clause['tag'] == 'Premise':
-                    file_dict['premises'].append(clause)
-                elif clause['tag'] == 'Claim':
-                    file_dict['claims'].append(clause)
-                file_dict['blocks'].append(block_list)
+                # Retrieve clauses of tokens with consecutive label.
+                clause, stack = processing_helper(stack, clause_counter)
+                clause_counter += 1
+
+                if clause["label"] == "Premise":
+                    file_dict["premises"].append(clause)
+                elif clause["label"] == "Claim":
+                    file_dict["claims"].append(clause)
+                file_dict["blocks"].append(clause)
+
             results.append(file_dict)
     return results
 
