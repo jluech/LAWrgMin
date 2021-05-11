589d388b82eeb0faac6fcb57e3ae5addc56e1835
Janik Lüchinger <janik.luechinger@uzh.ch>
2021-05-11 18:50:03 +0200

    refactor: replace br with spacing

--- frontend/src/components/report/report-section.jsx
+++ frontend/src/components/report/report-section.jsx
@@ -10,37 +10,33 @@ export class ReportSection extends React.Component {
     render() {
         return (
             <div id="project">
-                <div className="light">
+                <div className="chapter light">
                     <h2 className="section-title">{report[0].section}</h2>
                     <div className="textbox">
                         <img className="text-image" src={image1} alt={""} />
                         <p className="text" >{report[0].text}</p>
                     </div>
-                    <br/>
                 </div>
-                <div className="dark">
+                <div className="chapter dark">
                     <h2 className="section-title">{report[1].section}</h2>
                     <div className="textbox">
                         <img className="text-image" src={image2} alt={""} />
                         <p className="text">{report[1].text}</p>
                     </div>
-                    <br/>
                 </div>
-                <div className="light">
+                <div className="chapter light">
                     <h2 className="section-title">{report[2].section}</h2>
                     <div className="textbox">
                         <img className="text-image" src={imagefront} alt={""} />
                         <p className="text">{report[2].text}</p>
                     </div>
-                    <br/>
                 </div>
-                <div className="dark">
+                <div className="chapter dark">
                     <h2 className="section-title">{report[3].section}</h2>
                     <div className="textbox">
                         <img className="text-image" src={image2} alt={""} />
                         <p className="text">{report[3].text}</p>
                     </div>
-                    <br/>
                 </div>
             </div>
         );
